dcf122e7cddf9bc0069fbae2d5c49912d1cb13b7
Hugo Jeller Ferreira <hugo_jeller@hotmail.com>
2021-10-15 19:21:50 -0400

    Remove authentication stuff from backend

--- src/handlers/auth/index.ts
+++
@@ -1,6 +0,0 @@
-import "reflect-metadata";
-import 'source-map-support/register'
-import {generateHandler} from "../../helpers/handler-helpers";
-import {AuthHandler} from "./auth-handler";
-
-module.exports.auth = generateHandler(AuthHandler)
